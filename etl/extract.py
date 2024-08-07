a77c63db1882381da1b40500d687832c390e26ce
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-07 11:41:27 -0400

    Create extract.py

---
+++ etl/extract.py
@@ -0,0 +1,6 @@
+# importing necessary libraries
+import pandas as pd
+
+# Dataset used on Medicare.gov for hospital quality comparison
+hosp_df = pd.read_csv('Hospital_General_Ratings.csv', encoding = 'latin1')
+
