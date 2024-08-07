aa4af2200c7b452f6fe3a972e9c4614914ad7323
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-07 13:43:33 -0400

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Cleaning the Data
 
 # Selecting only columns necessary for analysis
-hosp_df_cleaned = hosp_df[['State', 'Readmission national comparison', 'Patient experience national comparison', 'Effectiveness of care national comparison', 'Timeliness of care national comparison', 'Efficient use of medical imaging national comparison']]
+hosp_df_cleaned = hosp_df[['Readmission national comparison', 'Patient experience national comparison', 'Effectiveness of care national comparison', 'Timeliness of care national comparison', 'Efficient use of medical imaging national comparison']]
 
 # Replacing "Not Available" with null to drop the values
 hosp_df_cleaned = hosp_df_cleaned.replace('Not Available', pd.NA)
@@ -12,3 +12,5 @@ hosp_df_cleaned = hosp_df_cleaned.dropna()
 
 # returning cleaned dataframe
 hosp_df_cleaned
+
+
