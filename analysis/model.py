ce102aff1ed5702ea8cb0ec01afce9b25a5c6837
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-07 20:03:29 -0400

    Update model.py

--- analysis/model.py
+++ analysis/model.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 import data.loaded
 
@@ -37,9 +36,3 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 y_pred_lr = lr_model.predict(X_test)
-
-
-# Random Forest Regressor model
-rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
-rf_model.fit(X_train, y_train)
-y_pred_rf = rf_model.predict(X_test)
