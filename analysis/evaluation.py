6dfaadaf11a810c9fc430e86b4a09be638daab98
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-07 20:02:45 -0400

    Update evaluation.py

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -6,10 +6,3 @@ mse_lr = mean_squared_error(y_test, y_pred_lr)
 r2_lr = r2_score(y_test, y_pred_lr)
 
 print(f'Linear Regression MSE: {mse_lr}, R2: {r2_lr}')
-
-# Random Forest Regressor Model Evaluation:
-mse_rf = mean_squared_error(y_test, y_pred_rf)
-r2_rf = r2_score(y_test, y_pred_rf)
-
-print(f'Random Forest MSE: {mse_rf}, R2: {r2_rf}')
-
