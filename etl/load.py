478c99e21710a3b9b1df8c58180676ff928502db
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-07 21:45:19 -0400

    Update load.py

--- etl/load.py
+++ etl/load.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import data.extracted
-import data.transformed
+import etl.transform
 
 # Load cleaned data into a new CSV
 hosp_df_cleaned.to_csv('Hospital_General_Ratings_Cleaned.csv', index=False)
