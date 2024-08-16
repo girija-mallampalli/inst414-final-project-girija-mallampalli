179a3176baad60b60d5212089b36908a3e6c5a6a
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-16 16:16:44 -0400

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -1,17 +1,28 @@
 import pandas as pd
-import etl.extract
+import logging
+from sklearn.preprocessing import LabelEncoder
+from etl import extract
 
 
-# Cleaning the Data
+def transform(hosp_df):
+    try:
+        logging.info('Started data transformation')
+        hosp_df_cleaned = hosp_df[['State', 'Readmission national comparison', 'Patient experience national comparison', 
+                                   'Effectiveness of care national comparison', 'Timeliness of care national comparison', 
+                                   'Efficient use of medical imaging national comparison']]
+        hosp_df_cleaned = hosp_df_cleaned.replace('Not Available', pd.NA)
+        hosp_df_cleaned = hosp_df_cleaned.dropna()
 
-# Selecting only columns necessary for analysis
-hosp_df_cleaned = hosp_df[['Readmission national comparison', 'Patient experience national comparison', 'Effectiveness of care national comparison', 'Timeliness of care national comparison', 'Efficient use of medical imaging national comparison']]
+        # Encode categorical variables
+        label_encoder = LabelEncoder()
+        columns_to_encode = ['Effectiveness of care national comparison', 'Readmission national comparison', 
+                             'Timeliness of care national comparison', 'Efficient use of medical imaging national comparison', 
+                             'Patient experience national comparison']
+        for column in columns_to_encode:
+            hosp_df_cleaned[column] = label_encoder.fit_transform(hosp_df_cleaned[column].astype(str))
 
-# Replacing "Not Available" with null to drop the values
-hosp_df_cleaned = hosp_df_cleaned.replace('Not Available', pd.NA)
-hosp_df_cleaned = hosp_df_cleaned.dropna()
-
-# returning cleaned dataframe
-hosp_df_cleaned
-
-
+        logging.info('Data transformation completed successfully')
+        return hosp_df_cleaned
+    except Exception as e:
+        logging.error(f'Error during data transformation: {e}')
+        raise
