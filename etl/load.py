98eff70f3e8e34ebba40cc5e41424b5559933d7d
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-16 19:43:34 -0400

    Update load.py

--- etl/load.py
+++ etl/load.py
@@ -1,13 +1,20 @@
 import os
 import logging
 import pandas as pd
-from etl import extract, transform
+from data import extracted, transformed
 
-def load(hosp_df_cleaned):
+# Setting up logging for load.py
+logging.basicConfig(
+    filename='data/logs/load.log',  # Log file specific to the load.py script
+    level=logging.DEBUG,            # Log level set to DEBUG to capture all messages
+    format='%(asctime)s %(levelname)s %(message)s'  # Log format with timestamp, level, and message
+)
+
+def load_data(hosp_df_cleaned):
     try:
         logging.info('Started data loading')
-        os.makedirs('data/cleaned', exist_ok=True)
-        hosp_df_cleaned.to_csv('data/hosp_df_cleaned.csv', index=False)
+        os.makedirs('data/loaded', exist_ok=True)
+        hosp_df_cleaned.to_csv('data/loaded/hosp_df_cleaned.csv', index=False)
         logging.info('Data loading completed successfully')
     except Exception as e:
         logging.error(f'Error during data loading: {e}')
