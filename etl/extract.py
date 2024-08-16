de2347fa467a2bcb2fa3b9b3ba321491bde969df
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-16 16:15:27 -0400

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -1,18 +1,13 @@
 # importing necessary libraries
+import logging
 import pandas as pd
 
-# Dataset used on Medicare.gov for hospital quality comparison
-hosp_df = pd.read_csv('Hospital_General_Ratings.csv', encoding = 'latin1')
-
-# Define the new folder path
-new_folder_path = 'data/extracted'
-
-# Check if the folder exists, if not, create it
-if not os.path.exists(new_folder_path):
-    os.makedirs(new_folder_path)
-
-# Define the full file path including the filename
-file_path = os.path.join(new_folder_path, 'Hospital_General_Ratings.csv')
-
-# Export the DataFrame to the specified file
-hosp_df.to_csv(file_path, index=False)
+def extract():
+    try:
+        logging.info('Started data extraction')
+        hosp_df = pd.read_csv('Hospital_General_Ratings.csv', encoding='latin1')
+        logging.info('Data extraction completed successfully')
+        return hosp_df
+    except Exception as e:
+        logging.error(f'Error during data extraction: {e}')
+        raise
