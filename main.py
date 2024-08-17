c25117ae54be7672973b902be998d4d3d2748167
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-16 20:11:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import logging
-from etl import extract, transform, load
-from modeling import train_model
-from evaluation import evaluate_and_visualize
+from analysis import extract, transform, load
+from model import train_random_forest
+from evaluate import evaluate
+from visualizations import visualize
+
 
 def main():
     """
-    The main function orchestrates the ETL process, model training, and evaluation.
-    It calls functions from the ETL, modeling, and evaluation modules to process data,
-    train models, and generate performance metrics and visualizations.
+    Orchestrates the entire data processing pipeline, including ETL (Extract, Transform, Load),
+    model training, evaluation, and visualization.
+
+    Raises:
+        Exception: If any step in the pipeline fails, an exception is logged and re-raised.
     """
     try:
-        logging.info('Started the main function')
+        logging.info('Started the main execution')
 
         # ETL Process
         hosp_df = extract()
         hosp_df_cleaned = transform(hosp_df)
         load(hosp_df_cleaned)
 
-        # Modeling
-        results = train_model(hosp_df_cleaned)
+        # Model Training
+        model_results = train_random_forest(hosp_df_cleaned)
 
-        # Evaluation and Visualization
-        evaluate_and_visualize(results)
+        # Evaluation
+        evaluate(model_results)
 
-        logging.info('Finished the main function successfully')
+        # Visualization
+        visualize()
+
+        logging.info('Main execution completed successfully')
+
     except Exception as e:
-        logging.error(f'Unexpected error in main function: {e}')
-
+        logging.error(f'Error during main execution: {e}')
+        raise
+        
 if __name__ == '__main__':
     main()
