066a4a1f6d5b481f54c5813d9dfa5583ed0745a9
Girija Mallampalli <107267588+girija-mallampalli@users.noreply.github.com>
2024-08-16 20:42:46 -0400

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -1,34 +1,36 @@
 # importing necessary libraries
+import os
 import logging
 import pandas as pd
+
 
 # Setting up logging
 logging.basicConfig(filename='data/logs/extract.log', level=logging.DEBUG, 
                     format='%(asctime)s %(levelname)s %(message)s')
 
 def extract():
-  """
-    Extracts hospital quality data from a CSV file.
-
-    Reads the 'Hospital_General_Ratings.csv' file, which contains various metrics related to hospital
-    performance and patient care.
+    """
+    Extracts the raw hospital ratings data from the CSV file in the 'data/' folder.
 
     Returns:
-        pd.DataFrame: The raw hospital data as a Pandas DataFrame.
-
-    Raises:
-        Exception: If the file cannot be read, logs the error and re-raises the exception.
+        pd.DataFrame: A DataFrame containing the extracted data.
     """
     try:
         logging.info('Started data extraction')
-        hosp_df = pd.read_csv('Hospital_General_Ratings.csv', encoding='latin1')
-        logging.info('Data extraction completed successfully')
-
-        # Save the extracted DataFrame
-        os.makedirs('data/extracted', exist_ok=True)
-        hosp_df.to_csv('data/extracted/hosp_df.csv', index=False)
-        logging.info('Extracted data saved successfully')
+        # Path to the raw CSV file in the 'data/' folder
+        file_path = 'data/Hospital General Ratings.csv'
         
+        # Load the dataset
+        hosp_df = pd.read_csv(file_path, encoding='latin1')
+        
+         # Ensure the 'data/extracted' directory exists
+        os.makedirs('data/extracted', exist_ok=True)
+        
+        # Save the extracted data to the 'data/extracted/' folder
+        extracted_file_path = 'data/extracted/hospital_general_ratings.csv'
+        hosp_df.to_csv(extracted_file_path, index=False)
+        
+        logging.info(f'Data extraction completed successfully and saved to {extracted_file_path}')
         return hosp_df
     except Exception as e:
         logging.error(f'Error during data extraction: {e}')
